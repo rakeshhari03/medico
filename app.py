b736cf0d67f321b65105163f5efc5ac044329d3c
rakesh hari <rakeshharierv@gmail.com>
2026-02-17 23:23:01 +0530

    "The home page and home function

---
+++ app.py
@@ -0,0 +1,19 @@
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+
+app=FastAPI()
+
+templates=Jinja2Templates(directory="templates")
+
+
+@app.get("/",response_class=HTMLResponse)
+def home(request:Request):
+    return templates.TemplateResponse("home.html",{"request":request})
+
+
+
+
+if __name__=="__main__":
+    import uvicorn
+    uvicorn.run(app,host="127.0.0.1",port=8000)
